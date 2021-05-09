11766879bc2daf78d01d4efcee8e823160318814
seidsid <seidsidseid@gmail.com>
2021-05-09 17:57:11 -0500

    initial commit

---
+++ src/main/java/com/maharishi/may/ereverse/ereverseauctionsystem/EReverseAuctionSystemApplication.java
@@ -0,0 +1,13 @@
+package com.maharishi.may.ereverse.ereverseauctionsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EReverseAuctionSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EReverseAuctionSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/maharishi/may/ereverse/ereverseauctionsystem/EReverseAuctionSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maharishi.may.ereverse.ereverseauctionsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EReverseAuctionSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
